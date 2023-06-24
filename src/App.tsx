aa12cafef152a3761765ccbae59bd991d36270ea
DanielLaPrade1 <lapraddx@dukes.jmu.edu>
2023-06-24 15:53:15 -0400

    chakra test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid templateAreas={`"nav nav" "aside main"`}>
+      <GridItem area="nav" bg="coral"></GridItem>
+      <GridItem area="aside" bg="gold"></GridItem>
+      <GridItem area="main" bg="dodgerblue"></GridItem>
+    </Grid>
   );
 };
 
